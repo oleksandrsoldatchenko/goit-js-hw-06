b4e53a71d304ddbd921ad2d189ca492f2c9bb36c
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-10 15:51:32 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const listIngredients = ingredients.map((ingredient) => {
+  const itemIngredient = document.createElement('li');
+  itemIngredient.classList.add('item');
+  itemIngredient.textContent = ingredient;
+  return itemIngredient;
+});
+
+ingredientsEl.append(...listIngredients);
