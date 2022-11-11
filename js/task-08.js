96426321b5ec6b277700efc0d27ffa25e9136f3a
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-11 13:56:21 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Login or password fields are not filled");
+    }
+
+    const result = {
+        email: email.value,
+        password: password.value };
+    event.currentTarget.reset();
+};
