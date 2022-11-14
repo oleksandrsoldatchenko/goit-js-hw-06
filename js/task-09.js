16820893229d08d3dce55a166e11442fa0ef8a1e
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-14 10:52:46 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', changeBacgroundColor);
+
+function changeBacgroundColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+};
