8d2a8d10e7b64355980fb76741c729d4d477e911
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-15 12:12:15 +0200

    Fixed task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,19 +2,43 @@ const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", onFormSubmit);
 
+// -----------------------version 1
+// function onFormSubmit(event) {
+//     event.preventDefault();
+
+//     const {
+//         elements: { email, password },
+//     } = event.currentTarget;
+
+//     if (email.value === "" || password.value === "") {
+//         return alert("Login or password fields are not filled");
+//     }
+
+//     const result = {
+//         email: email.value,
+//         password: password.value };
+//     event.currentTarget.reset();
+// };
+
+
+// -----------------------version 2
 function onFormSubmit(event) {
     event.preventDefault();
 
-    const {
-        elements: { email, password },
-    } = event.currentTarget;
+    const onFormSubmitEl = event.currentTarget.elements;
+    const emailEl = onFormSubmitEl.email.value;
+    const passwordEl = onFormSubmitEl.password.value;
 
-    if (email.value === "" || password.value === "") {
+    if (onFormSubmitEl.email.value === "" || onFormSubmitEl.password.value === "") {
         return alert("Login or password fields are not filled");
     }
 
     const result = {
-        email: email.value,
-        password: password.value };
+        emailEl,
+        passwordEl,
+    };
+
+    console.log(result);
+
     event.currentTarget.reset();
 };
