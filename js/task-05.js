c9ecb7e6eccb497fed44a5fa99f09404474da594
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-15 11:45:11 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -5,9 +5,19 @@ const refs = {
 
 refs.inputEl.addEventListener('input', spanChange);
 
+// -----------------------version 1
+// function spanChange(event) {
+//     refs.spanEl.textContent = event.currentTarget.value;
+//     if (event.currentTarget.value === "") {
+//         spanEl.textContent = 'Anonymous'
+//     }
+// };
+
+// -----------------------version 2
 function spanChange(event) {
-    refs.spanEl.textContent = event.currentTarget.value;
-    if (event.currentTarget.value === "") {
-        spanEl.textContent = 'Anonymous'
+    const spanChangeCheck = event.currentTarget.value;
+    if (spanChangeCheck.length === 0) {
+        return refs.spanEl.textContent = 'Anonymous'
     }
+    refs.spanEl.textContent = event.currentTarget.value;
 };
