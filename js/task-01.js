8ec82e00a2ae4fc4de752c24747f4b401cbc38d1
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-15 10:50:34 +0200

    Fixed task-01.html

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,22 @@
-const arrayCategories = document.querySelectorAll('.item');
-console.log(`Number of categories: ${arrayCategories.length}`);
+// -----------------------version 1
+// const arrayCategories = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${arrayCategories.length}`);
 
-arrayCategories.forEach((item) => {
-    console.log('');
-    console.log(`Category: ${item.firstElementChild.textContent}`);
-    console.log(`Elements: ${item.lastElementChild.children.length}`);
+// arrayCategories.forEach((item) => {
+//     console.log('');
+//     console.log(`Category: ${item.firstElementChild.textContent}`);
+//     console.log(`Elements: ${item.lastElementChild.children.length}`);
+// });
+
+// -----------------------version 2
+const arrayCategories = document.querySelector('#categories');
+const numberOfCategories = arrayCategories.children;
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+const categoriesList = document.querySelectorAll('.item');
+
+categoriesList.forEach(item => {
+  console.log('');
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
